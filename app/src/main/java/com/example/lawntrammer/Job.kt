049ee06482ab1e3c7248be7eb56b6cba28b29311
AZain99 <alizainmurad99@gmail.com>
2026-02-15 00:21:24 +0500

    apk added

--- app/src/main/java/com/example/lawntrammer/Job.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lawntrammer
-
-    data class Job(
-        val title: String,
-        val location: String,
-        val time: String,
-        val status: String
-    )
--- app/src/main/java/com/example/lawntrammer/Service.kt
+++
@@ -1,3 +0,0 @@
-package com.example.lawntrammer
-
-data class Service(val name: String, val icon: Int)
